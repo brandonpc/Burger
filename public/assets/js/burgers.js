f0cfb1fc7bade7a9d03b17ac69d418323e57a369
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-07-02 18:00:56 -0600

    updated server.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,6 +1,6 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-  $(".change-sleep").on("click", function(event) {
+$(function () {
+  $(".change-sleep").on("click", function (event) {
     var id = $(this).data("id");
     var newSleep = $(this).data("newsleep");
 
@@ -9,51 +9,51 @@ $(function() {
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "PUT",
       data: newSleepState
     }).then(
-      function() {
+      function () {
         console.log("changed sleep to", newSleep);
         // Reload the page to get the updated list
-        location.reload();
+        loburgerion.reload();
       }
     );
   });
 
-  $(".create-form").on("submit", function(event) {
+  $(".create-form").on("submit", function (event) {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newburger = {
       name: $("#ca").val().trim(),
       sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newburger
     }).then(
-      function() {
-        console.log("created new cat");
+      function () {
+        console.log("created new burger");
         // Reload the page to get the updated list
-        location.reload();
+        loburgerion.reload();
       }
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-burger").on("click", function (event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
-      function() {
-        console.log("deleted cat", id);
+      function () {
+        console.log("deleted burger", id);
         // Reload the page to get the updated list
-        location.reload();
+        loburgerion.reload();
       }
     );
   });
--- server.js
+++ server.js
@@ -1,29 +1,43 @@
+// req express and handlebars
 var express = require("express");
-
-var PORT = process.env.PORT || 8080;
-
 var app = express();
-
+var exphbs = require("express-handlebars");
+// use routes.js 
+var routes = require("./controllers/burgersController.js");
+// set port
+var PORT = process.env.PORT || 8080;
+// req suggested middlewares
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
-
+// use middlewares
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(methodOverride('_method'));
 // Parse application body as JSON
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Set Handlebars.
-var exphbs = require("express-handlebars");
+app.use(bodyParser.urlencoded({
+  endtended: false
+}));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+
+
+// Set Handlebars.
+
+app.engine("handlebars", exphbs({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/catsController.js");
 
 app.use(routes);
 
 // Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   // Log (server-side) when our server has started
   console.log("Server listening on: http://localhost:" + PORT);
 });
