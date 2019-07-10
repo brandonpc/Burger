819ba4eff7fd7ab26955c5ea32d5993b331344ad
brandonconover90@gmail.com <brandonconover90@gmail.com>
2019-07-09 23:22:44 -0600

    heroku deployment

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   // port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "eatdaburger_db"
+// });
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  // port: 3306,
-  user: "root",
-  password: "Blue2008-sql",
-  database: "eatdaburger_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "umabrisfx8afs3ja.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "	c7vq0qrrtm5zffav",
+    password: "jxsnudm51py1pi8t",
+    port: 3306,
+    database: "xb7bn0wnbkkg0h9k"
+  })
+}
 
 connection.connect(function (err) {
   // if (err) {
